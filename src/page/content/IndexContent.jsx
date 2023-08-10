78743f8b6b1e09b2df581733f02dfc99f7e217ab
Sabda1110 <sabdasetiawan206@gmail.com>
2023-08-10 14:52:11 +0700

    Memperbaiki tampilan

--- src/page/content/IndexContent.jsx
+++ src/page/content/IndexContent.jsx
@@ -1,7 +1,6 @@
 import Navbar from '../../Components/navbar/Navbar';
 import Footer from '../../Components/footer/Footer';
 import ContentPertama from './ContentPertama';
-import ContentKedua from './contentKedua';
 import ContentKetiga from './ContentKetiga';
 import { Routes, Route } from 'react-router-dom';
 import './content.css';
@@ -12,7 +11,6 @@ const IndexContent = () => {
       <Navbar />
       <Routes>
         <Route path="/blog/contentPertama" element={<ContentPertama />} />
-        <Route path="/blog/kedua" element={<ContentKedua />} />
         <Route path="/blog/ketiga" element={<ContentKetiga />} />
       </Routes>
       <Footer />
